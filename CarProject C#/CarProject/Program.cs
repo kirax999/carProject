b8136273cb07a6ebea8cf16043d237cd4ed4af4a
Alexandre Richet <alexandre.richet@epitech.eu>
2017-12-09 02:04:53 +0100

    Add C# Mod

---
+++ CarProject C#/CarProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+
+namespace CarProject
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            CameraCapture();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static void CameraCapture()
+        {
+            ImageViewer viewer = new ImageViewer(); //create an image viewer
+            Capture capture = new Capture(); //create a camera captue
+            Application.Idle += new EventHandler(delegate (object sender, EventArgs e)
+            {  //run this until application closed (close button click on image viewer)
+                viewer.Image = capture.QueryFrame(); //draw the image obtained from camera
+            });
+            viewer.ShowDialog();
+        }
+    }
+}
